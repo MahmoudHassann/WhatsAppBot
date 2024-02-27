555416ca4ace1241ca1efe7cd4ab0f2e190380d2
Mahmoud Mohamed <mahmoud.m.azab@gmail.com>
2024-02-27 17:08:17 +0200

    port

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const { dirname } = require('path');
 
 const app = express()
 app.use(express.urlencoded({ extended: true }));
-const port = 3000
+const port = 80
 
 
 const __filenameCommonJS = __filename;  // Use __filename directly
